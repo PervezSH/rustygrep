0f08c6ffd0321d532614c048670d815c2c108613
PervezSH <50441829+PervezSH@users.noreply.github.com>
2022-05-12 15:50:16 +0530

    fix: error handling while parsing args 🥅

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use std::env; // to use args function provided by rust's standard library
 use std::fs; // to handle files
+use std::process; // to exit program without panicking
+
+fn main() {
+    let args: Vec<String> = env::args().collect(); // collect() turns iterator into vector
+    let params = Params::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    print!("Searching for {} in file {}", params.query, params.file);
+
+    let contents =
+        fs::read_to_string(params.file).expect("Something went wrong while reading the file!");
+    print!(" with text: \n{}", contents);
+}
 
 struct Params<'a> {
     query: &'a String,
@@ -7,20 +22,12 @@ struct Params<'a> {
 }
 
 impl<'a> Params<'a> {
-    fn new(args: &[String]) -> Params {
+    fn new(args: &[String]) -> Result<Params, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = &args[1];
         let file = &args[2];
-        Params { query, file }
+        Ok(Params { query, file })
     }
-}
-
-fn main() {
-    let args: Vec<String> = env::args().collect(); // collect() turns iterator into vector
-    let params = Params::new(&args);
-
-    print!("Searching for {} in file {}", params.query, params.file);
-
-    let contents =
-        fs::read_to_string(params.file).expect("Something went wrong while reading the file!");
-    print!(" with text: \n{}", contents);
 }
