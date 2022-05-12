c2dc10cdd59ca08959d7de8a21872019c9505cf1
PervezSH <50441829+PervezSH@users.noreply.github.com>
2022-05-12 16:20:35 +0530

    test: add test for search function 🧪

--- src/lib.rs
+++ src/lib.rs
@@ -24,3 +24,17 @@ impl<'a> Params<'a> {
         Ok(Params { query, file })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
