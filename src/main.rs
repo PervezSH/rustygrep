01313288fd11cb9619e41b1d98b4a06a61d83384
PervezSH <50441829+PervezSH@users.noreply.github.com>
2022-05-12 17:07:11 +0530

    feat: use stderr instead of stdout 👔

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use std::process; // to exit program without panicking
 fn main() {
     let args: Vec<String> = env::args().collect(); // collect() turns iterator into vector
     let params = Params::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     println!("Searching for {} in {}...", params.query, params.file);
     if let Err(e) = rustygrep::run(params) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
