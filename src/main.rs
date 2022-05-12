e5fd89fe5c06060e4b095bc71d429d567f1776b6
PervezSH <50441829+PervezSH@users.noreply.github.com>
2022-05-12 14:56:18 +0530

    feat: accept command line args ✨

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env; // to use args function provided by rust's standard library
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect(); // collect() turns iterator into vector
+
+    let query = &args[1];
+    let file = &args[2];
+
+    println!("Searching for {} in file {}", query, file);
 }
