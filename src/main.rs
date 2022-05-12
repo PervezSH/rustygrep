77e17cf1ff83888bf4a89708a613909fc39f71a9
PervezSH <50441829+PervezSH@users.noreply.github.com>
2022-05-12 15:24:19 +0530

    refactor: input args ♻️

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 use std::env; // to use args function provided by rust's standard library
 use std::fs; // to handle files
 
+struct Params<'a> {
+    query: &'a String,
+    file: &'a String,
+}
+
+impl<'a> Params<'a> {
+    fn new(args: &[String]) -> Params {
+        let query = &args[1];
+        let file = &args[2];
+        Params { query, file }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect(); // collect() turns iterator into vector
+    let params = Params::new(&args);
 
-    let query = &args[1];
-    let file = &args[2];
+    print!("Searching for {} in file {}", params.query, params.file);
 
-    print!("Searching for {} in file {}", query, file);
-
-    let contents = fs::read_to_string(file).expect("Something went wrong while reading the file!");
+    let contents =
+        fs::read_to_string(params.file).expect("Something went wrong while reading the file!");
     print!(" with text: \n{}", contents);
 }
